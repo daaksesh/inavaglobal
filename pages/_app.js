4eeea89a49bfc99d873c4a5766b09c2182889457
daaksesh <daaksesh@gmail.com>
2025-08-28 18:02:10 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
+import { Manrope } from 'next/font/google'
+
+const manrope = Manrope({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className={manrope.className}><Component {...pageProps} /></div>
 }
