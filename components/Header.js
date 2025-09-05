9eb2afa9bb4af59ace4e5b4d314fed7f098444ff
daaksesh <daaksesh@gmail.com>
2025-09-05 13:21:52 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -33,7 +33,7 @@ export default function Header() {
           href="tel:+919004665866"
           className="hidden md:inline-flex items-center justify-center rounded-xl bg-gold px-5 py-3 text-navy font-semibold shadow hover:bg-[#E6CF84] transition"
         >
-          "📞Call Us Now"
+          📞Call Us Now
         </a>
 
         {/* Mobile Hamburger */}
@@ -68,7 +68,7 @@ export default function Header() {
                 href="tel:+919004665866"
                 className="inline-flex items-center justify-center rounded-xl bg-gold px-5 py-3 text-navy font-semibold shadow hover:bg-[#E6CF84] transition"
               >
-                "📞Call Us Now"
+                📞Call Us Now
               </a>
             </div>
           </div>
